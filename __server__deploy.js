895c6a09a114d6ad1dd545cfc8b2d0aaa452332f
sadik <po480373@gmai.com>
2022-05-08 18:22:16 +0600

    server test deploy

---
+++ __server__deploy.js
@@ -0,0 +1,15 @@
+/**
+ * ONE TIME
+ * 1. create heroku account
+ * 2. verify email
+ * 3. install heroku cli
+ * 4. heroku login from terminal
+------------------------------ 
+for each project one time
+------------------------------- 
+1. heroku create
+
+
+
+
+*/
